101238b5e07c874e621f3c1432521536e25a94c8
elseElseIf <mihail.pryka@gmail.com>
2023-07-21 17:34:16 +0300

    feat: add main html and css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,94 @@
+import "./styles/main.css";
+import sun from "./img/icons/sun.svg";
+import moon from "./img/icons/moon.svg";
+import gitHub from "./img/icons/gitHub.svg";
+import linkedIn from "./img/icons/linkedIn.svg";
+
 function App() {
   return (
     <div className="App">
-      <p>Test</p>
+      <nav className="nav">
+        <div className="container">
+          <div className="nav-row">
+            <a href="./index.html" className="logo">
+              <strong>Freelancer</strong> portfolio
+            </a>
+
+            <button className="dark-mode-btn">
+              <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />{" "}
+              <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />{" "}
+            </button>
+
+            <ul className="nav-list">
+              <li className="nav-list__item">
+                <a
+                  href="./index.html"
+                  className="nav-list__link nav-list__link--active"
+                >
+                  Home
+                </a>
+              </li>
+              <li className="nav-list__item">
+                <a href="./projects.html" className="nav-list__link">
+                  Projects
+                </a>
+              </li>
+              <li className="nav-list__item">
+                <a href="./contacts.html" className="nav-list__link">
+                  Contacts
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <header className="header">
+        <div className="header__wrapper">
+          <h1 className="header__title">
+            <strong>
+              Hi, my name is <em>Mikhail</em>
+            </strong>
+            <br></br>a frontend developer
+          </h1>
+          <div className="header__text">
+            <p>with passion for learning and creating.</p>
+          </div>
+          <a href="#!" className="btn">
+            Download CV
+          </a>
+        </div>
+      </header>
+      <main className="section">
+        <div className="container">
+          <ul className="content-list">
+            <li className="content-list__item">
+              <h2 className="title-2">Frontend</h2>
+              <p>JavaScript, TypeScript, ReactJS,, HTML, CSS, NPM, BootStrap</p>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <footer className="footer">
+        <div className="container">
+          <div className="footer__wrapper">
+            <ul className="social">
+              <li className="social__item">
+                <a href="#!">
+                  <img src={gitHub} alt="Link"></img>
+                </a>
+              </li>
+              <li className="social__item">
+                <a href="#!">
+                  <img src={linkedIn} alt="Link"></img>
+                </a>
+              </li>
+            </ul>
+            <div className="copyright">
+              <p>© 2023</p>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }

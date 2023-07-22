bf243e230480fece977dadf4df76156aac1759a9
elseElseIf <mihail.pryka@gmail.com>
2023-07-22 19:40:11 +0300

    feat: add Projects component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./styles/main.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <Projects />
       <Footer />
     </div>
   );

68059ee0602d2efbb75175234a41ccd72cad0373
elseElseIf <mihail.pryka@gmail.com>
2023-07-22 19:43:36 +0300

    feat: add Contacts component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
+import Contacts from "./pages/Contacts";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Home />
       <Projects />
+      <Contacts />
       <Footer />
     </div>
   );
---
+++ src/pages/Contacts.js
@@ -0,0 +1,30 @@
+const Contacts = () => {
+  return (
+    <main className="section">
+      <div className="container">
+        <h1 className="title-1">Contacts</h1>
+
+        <ul className="content-list">
+          <li className="content-list__item">
+            <h2 className="title-2">Location</h2>
+            <p>Warszawa, Poland</p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Telegram / WhatsApp</h2>
+            <p>
+              <a href="tel:+79051234567">+48 726-274-617</a>
+            </p>
+          </li>
+          <li className="content-list__item">
+            <h2 className="title-2">Email</h2>
+            <p>
+              <a href="mailto:trappistw12@gmail.com">trappistw12@gmail.com</a>
+            </p>
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+};
+
+export default Contacts;

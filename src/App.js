acba71267eadd940fed8f5de4ccc141514cc22cf
elseElseIf <mihail.pryka@gmail.com>
2023-07-22 19:30:25 +0300

    feat: add Home component

--- src/App.js
+++ src/App.js
@@ -1,45 +1,14 @@
 import "./styles/main.css";
-import gitHub from "./img/icons/gitHub.svg";
-import linkedIn from "./img/icons/linkedIn.svg";
 import Navbar from "./components/navbar/Navbar";
-import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Header />
-      <main className="section">
-        <div className="container">
-          <ul className="content-list">
-            <li className="content-list__item">
-              <h2 className="title-2">Frontend</h2>
-              <p>JavaScript, TypeScript, ReactJS,, HTML, CSS, NPM, BootStrap</p>
-            </li>
-          </ul>
-        </div>
-      </main>
-      <footer className="footer">
-        <div className="container">
-          <div className="footer__wrapper">
-            <ul className="social">
-              <li className="social__item">
-                <a href="#!">
-                  <img src={gitHub} alt="Link"></img>
-                </a>
-              </li>
-              <li className="social__item">
-                <a href="#!">
-                  <img src={linkedIn} alt="Link"></img>
-                </a>
-              </li>
-            </ul>
-            <div className="copyright">
-              <p>© 2023</p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,31 @@
+import "./style.css";
+import gitHub from "./../../img/icons/gitHub.svg";
+import linkedIn from "./../../img/icons/linkedIn.svg";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer__wrapper">
+          <ul className="social">
+            <li className="social__item">
+              <a href="#!">
+                <img src={gitHub} alt="Link"></img>
+              </a>
+            </li>
+            <li className="social__item">
+              <a href="#!">
+                <img src={linkedIn} alt="Link"></img>
+              </a>
+            </li>
+          </ul>
+          <div className="copyright">
+            <p>© 2023</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;

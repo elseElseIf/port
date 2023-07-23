7421b5e8e3f90a27503cb462aadb8fc08c38b793
elseElseIf <mihail.pryka@gmail.com>
2023-07-23 18:05:16 +0300

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -5,16 +5,23 @@ import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contacts from "./pages/Contacts";
 import Project from "./pages/Project";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Project />
-      {/* <Navbar />
-      <Home />
-      <Projects />
-      <Contacts />
-      <Footer /> */}
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+
+        <Footer />
+      </Router>
     </div>
   );
 }

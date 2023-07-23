8fefce05f92d0bb6865ec1f3d2aedd1b6ee04d3d
elseElseIf <mihail.pryka@gmail.com>
2023-07-23 22:27:19 +0300

    feat: add use effect and useRef from react

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,5 @@
-import sun from "./../../img/icons/sun.svg";
-import moon from "./../..//img/icons/moon.svg";
 import { NavLink } from "react-router-dom";
+import BtnDarkMode from "../btnDarkMode/BtnDarkMode";
 import "./style.css";
 
 const Navbar = () => {
@@ -14,12 +13,7 @@ const Navbar = () => {
           <NavLink to="/" className="logo">
             <strong>Freelancer</strong> portfolio
           </NavLink>
-
-          <button className="dark-mode-btn">
-            <img src={sun} alt="Light mode" className="dark-mode-btn__icon" />{" "}
-            <img src={moon} alt="Dark mode" className="dark-mode-btn__icon" />{" "}
-          </button>
-
+          <BtnDarkMode />
           <ul className="nav-list">
             <li className="nav-list__item">
               <NavLink

c771f5521aae307531d5b2246a33ae319a902179
elseElseIf <mihail.pryka@gmail.com>
2023-07-23 16:22:22 +0300

    feat: add main project page

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Footer from "./components/footer/Footer";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Contacts from "./pages/Contacts";
+import Project from "./pages/Project";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Project />
+      {/* <Navbar />
       <Home />
       <Projects />
       <Contacts />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
